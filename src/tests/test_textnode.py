03cea2a37e1a54e10dd904ec062342e4f8abe5bc
Sam Maton <sam.maton91@gmail.com>
2024-09-19 15:22:19 +0100

    trying to change file structure

--- src/tests/test_textnode.py
+++ src/tests/test_textnode.py
@@ -1,14 +1,14 @@
 import unittest
-
-from textnode import (
-    TextNode,
+from utils import(
     text_type_text,
     text_type_bold,
     text_type_italic,
     text_type_code,
     text_type_image,
     text_type_link,
+    text_node_to_html_node
 )
+from textnode import TextNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -38,6 +38,35 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", text_type_text)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", text_type_image, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", text_type_bold)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+    
+    def test_incorrect_type(self):
+        node = TextNode("This is bold", "incorrect")
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+
 
 if __name__ == "__main__":
     unittest.main()
