0c53c1b842374b0f60c1267ee47b4ca8be416a56
Sam Maton <sam.maton91@gmail.com>
2024-09-23 22:56:26 +0100

    can now convert text to nodes!

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,7 +1,19 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
-from utils import extract_markdown_images, extract_markdown_links
+from textnode import TextNode
+from utils import (
+    extract_markdown_images, 
+    extract_markdown_links, 
+    split_nodes_image, 
+    split_nodes_link,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+    )
 
 class TestExtractMarkdownImages(unittest.TestCase):
     def test_with_one_image(self):
@@ -34,3 +46,67 @@ class TestExtractMarkdownLinks(unittest.TestCase):
         text = "this is text with no links."
         extracted = extract_markdown_links(text)
         self.assertListEqual([], extracted)
+
+class TestExtractSplitNodesImage(unittest.TestCase):
+    def test_split_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", text_type_text),
+                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_image_single(self):
+        node = TextNode(
+            "![image](https://www.example.com/image.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("image", text_type_image, "https://www.example.com/image.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", text_type_text),
+                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", text_type_text),
+                TextNode(
+                    "second image", text_type_image, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+class TestExtractSplitNodesLink(unittest.TestCase):
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+                TextNode(" and ", text_type_text),
+                TextNode("another link", text_type_link, "https://blog.boot.dev"),
+                TextNode(" with text that follows", text_type_text),
+            ],
+            new_nodes,
+        )
--- src/utils.py
+++ src/utils.py
@@ -64,3 +64,84 @@ def extract_markdown_links(text):
     matches = re.findall(pattern, text)
 
     return matches
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type == text_type_text:
+            new_nodes += separate_links_from_text(node.text)
+        else:
+            new_nodes.append(node)
+
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type == text_type_text:
+            new_nodes += separate_images_from_text(node.text)
+        else:
+            new_nodes.append(node)
+
+    return new_nodes
+
+def separate_links_from_text(text):
+
+    if len(text) < 1:
+        return []
+    
+    links = extract_markdown_links(text)
+
+    if len(links) == 0:
+        return [TextNode(text, text_type_text)]
+    
+    for link in links:
+        new_link = TextNode(link[0], text_type_link, link[1])
+        sections = text.split(f"[{link[0]}]({link[1]})", 1)
+        if len(sections[0]) == 0:
+            return[new_link] + separate_links_from_text(sections[1])
+        else:
+            if len(sections[1]) == 0:
+                return[TextNode(sections[0], text_type_text), new_link]
+            return[TextNode(sections[0], text_type_text), new_link] + separate_links_from_text(sections[1])
+    return []
+
+def separate_images_from_text(text):
+
+    if len(text) < 1:
+        return []
+    
+    images = extract_markdown_images(text)
+
+    if len(images) == 0:
+        return [TextNode(text, text_type_text)]
+    
+    for image in images:
+        new_image = TextNode(image[0], text_type_image, image[1])
+        sections = text.split(f"![{image[0]}]({image[1]})", 1)
+        if len(sections[0]) == 0:
+            return[new_image] + separate_images_from_text(sections[1])
+        else:
+            if len(sections[1]) == 0:
+                return[TextNode(sections[0], text_type_text), new_image]
+            return[TextNode(sections[0], text_type_text), new_image] + separate_images_from_text(sections[1])
+    return []
+
+def text_to_textnodes(text):
+    starting_node = TextNode(text, text_type_text)
+    bold_nodes = split_nodes_delimiter([starting_node], '**', text_type_bold)
+    italic_nodes = split_nodes_delimiter(bold_nodes, '*', text_type_italic)
+    code_nodes = split_nodes_delimiter(italic_nodes, '`', text_type_code)
+    link_nodes = split_nodes_link(code_nodes)
+    image_nodes = split_nodes_image(link_nodes)
+
+    return(image_nodes)
+
+string = 'This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'
+
+converted = text_to_textnodes(string)
+
+for c in converted:
+    print(c)
