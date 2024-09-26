424cc0e6863805ef8450eb690aed235787b785b5
Sam Maton <sam.maton91@gmail.com>
2024-09-26 20:49:53 +0100

    added functionality to copy static files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,39 @@
+import os.path
 from textnode import TextNode
-def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+import os
+import shutil
 
-    print(node)
+def copy_static_files(path):
+    if not os.path.exists(path):
+        raise Exception('Invalid path')
+    
+    shutil.rmtree('public')
+    os.mkdir('public')
+    
+    def get_items_to_create(folder, files):
+        items = os.listdir(folder)
+
+        for i in items:
+            joined_path = os.path.join(folder + '/',i)
+
+            if os.path.isfile(joined_path):
+                files.append(joined_path)
+            else:
+                files.append(joined_path)
+                get_items_to_create(joined_path, files)
+        
+        return files
+
+    items: list[str] = get_items_to_create(path, [])
+
+    for i in items:
+        if os.path.isfile(i):
+            shutil.copy(i, f"public{i.removeprefix(path)}")
+        else:
+            os.mkdir(f"public{i.removeprefix(path)}")
+
+def main():
+    copy_static_files('static')
 
 if __name__ == "__main__":
     main()
